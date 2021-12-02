b8fdf9b0960845e044ea5fdcddd8f52c4534f302
Alex Vest <alex.vest@nutmeg.com>
2021-12-02 17:56:17 +0000

    Add notes

--- day1/day1p2.go
+++ day1/day1p2.go
@@ -6,67 +6,118 @@ import (
 )
 
 type window struct {
-	measurements []int
-	increase     int
+	measurements   []int
+	increase       int
+	previousWindow int
+}
+
+func (w *window) Populate(i int) {
+	// If we have less than 3 measurements in the window then append
+	if len(w.measurements) < 3 {
+		w.measurements = append(w.measurements, i)
+		return
+	}
+
+	// If we have more than 3 measurements then "move" the window
+	// and append the new measurement
+	w.measurements = w.measurements[1:]
+
+	w.measurements = append(w.measurements, i)
+}
+
+func (w *window) Sum() int {
+	// Calculate the sum for the current window
+	sum := 0
+	for _, m := range w.measurements {
+		sum += m
+	}
+
+	return sum
+
+}
+
+func (w *window) Compare() {
+	// If the previousWindow value is populated compare the current window
+	// sum against the previous window sum
+	if w.previousWindow != 0 {
+		if w.Sum() > w.previousWindow {
+			w.increase++
+		}
+	}
+
+	// Update the previous window sum to equal the current sum as the
+	// window will slide
+	w.previousWindow = w.Sum()
 }
 
 // A second implementation inspired by
-// https://github.com/MatthewJamesBoyle/adventOfCode2021/blob/main/day1/puzzle2.go
-func Day1P22(f string) {
+func main(f string) int {
 	scanner, close, err := fileReader(f)
 	defer close()
 	if err != nil {
-		return 0, err
+		fmt.Printf("could not open file, got err: %s", err)
 	}
 
+	w := &window{}
 	for scanner.Scan() {
 		measurement, err := strconv.Atoi(scanner.Text())
 		if err != nil {
-			return 0, fmt.Errorf("could not parse: %s to int, got err: %s", scanner.Text(), err)
+			fmt.Printf("could not parse: %s to int, got err: %s\n", scanner.Text(), err)
 		}
 
+		w.Populate(measurement)
+		if len(w.measurements) < 3 {
+			continue
+		}
+
+		w.Compare()
 	}
+
+	return w.increase
 }
 
-//--- Part Two ---
+// Imagine that we need to write a function that compares a sliding average of
+// measurements against one another.
 //
-//Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+// The size of each window will be 3 measurements, and we wish to record the
+// number of times the sum of measurements in each sliding window increases
+// compared to the last.
 //
-//Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+// Considering the above example:
 //
-//199  A
-//200  A B
-//208  A B C
-//210    B C D
-//200  E   C D
-//207  E F   D
-//240  E F G
-//269    F G H
-//260      G H
-//263        H
+// 199  A
+// 200  A B
+// 208  A B C
+// 210    B C D
+// 200  E   C D
+// 207  E F   D
+// 240  E F G
+// 269    F G H
+// 260      G H
+// 263        H
 //
-//Start by comparing the first and second three-measurement windows. The
-//measurements in the first window are marked A (199, 200, 208); their sum is
-//199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum
-//is 618. The sum of measurements in the second window is larger than the sum
-//of the first, so this first comparison increased.
+// Start by comparing the first and second three-measurement windows. The
+// measurements in the first window are marked A (199, 200, 208); their sum is
+// 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum
+// is 618. The sum of measurements in the second window is larger than the sum
+// of the first, so this first comparison increased.
 //
-//Your goal now is to count the number of times the sum of measurements in this
-//sliding window increases from the previous sum. So, compare A with B, then
-//compare B with C, then C with D, and so on. Stop when there aren't enough
-//measurements left to create a new three-measurement sum.
+// Your goal now is to count the number of times the sum of measurements in this
+// sliding window increases from the previous sum. So, compare A with B, then
+// compare B with C, then C with D, and so on. Stop when there aren't enough
+// measurements left to create a new three-measurement sum.
 //
-//In the above example, the sum of each three-measurement window is as follows:
+// In the above example, the sum of each three-measurement window is as follows:
 //
-//A: 607 (N/A - no previous sum)
-//B: 618 (increased)
-//C: 618 (no change)
-//D: 617 (decreased)
-//E: 647 (increased)
-//F: 716 (increased)
-//G: 769 (increased)
-//H: 792 (increased)
+// A: 607 (N/A - no previous sum)
+// B: 618 (increased)
+// C: 618 (no change)
+// D: 617 (decreased)
+// E: 647 (increased)
+// F: 716 (increased)
+// G: 769 (increased)
+// H: 792 (increased)
 //
-//In this example, there are 5 sums that are larger than the previous sum.
+// In this example, there are 5 sums that are larger than the previous sum.
 //
-//Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
--- day1/day1p2_test.go
+++ day1/day1p2_test.go
@@ -2,10 +2,87 @@ package day1
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestDay1P2(t *testing.T) {
+func TestAddMeasurement(t *testing.T) {
+	// Check that we can add measurements
+	measurements := []int{}
+	measurements = append(measurements, 0, 1, 2)
+	require.Equal(t, 0, measurements[0])
+	require.Equal(t, 1, measurements[2])
+	require.Equal(t, 2, measurements[2])
 
+	// What happens when we add a fourth measurement?
+	measurements = append(measurements, 3)
+	// We would expect the window to "slide" such that the previous first
+	// element is now the zeroth element
+	require.Equal(t, 1, measurements[0])
+	require.Equal(t, 3, measurements[2])
+}
+
+// When we add a fourth measurement does the window "slide"?
+func TestPopulate(t *testing.T) {
+	w := &window{}
+
+	w.Populate(0)
+	require.Equal(t, 0, w.measurements[0])
+	w.Populate(1)
+	require.Equal(t, 1, w.measurements[1])
+	w.Populate(2)
+	require.Equal(t, 2, w.measurements[2])
+
+	// What happens when we add a fourth measurement?
+	w.Populate(3)
+	require.Equal(t, 3, w.measurements[2])
+	// Did the window slide?
+	require.Equal(t, 1, w.measurements[0])
+}
+
+// When the window slides we need to know whether the new window has a greater
+// total than the previous window
+func TestSum(t *testing.T) {
+	w := &window{}
+
+	w.Populate(0)
+	w.Populate(1)
+	w.Populate(2)
+
+	got := w.Sum()
+	require.Equal(t, 3, got)
+
+	// What happens when we add another element and sum?
+	w.Populate(3)
+	got = w.Sum()
+	require.Equal(t, 6, got)
+}
+
+// Now that we have the window sliding and we can calculate window sums we need
+// to compare the current window total against the previous total
+func TestCompare(t *testing.T) {
+	w := &window{}
+
+	// Validate that the previousWindow is set when we hit 3 items
+	w.Populate(0)
+	w.Populate(1)
+	w.Populate(2)
+	w.Compare()
+	require.Equal(t, 3, w.previousWindow)
+
+	// Validate that the previousWindow is updated
+	// and that the
+	w.Populate(3)
+	w.Compare()
+	require.Equal(t, 6, w.previousWindow)
+	require.Equal(t, 1, w.increase)
+}
+
+// Now we can wire everything together
+func TestMain(t *testing.T) {
+	got := main("./day1_test.txt")
+
+	require.Equal(t, 5, got)
 }
 
 // Answer for the below is 5

467452d1243aa7d4d3e4d1ed17efc9652b644378
Alex Vest <alex.vest@nutmeg.com>
2021-12-02 18:01:18 +0000

    Try to make main work

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,18 @@ package main
 import (
 	"fmt"
 
-	"github.com/avestuk/aoc2021/day1"
+	"github.com/avestuk/aoc2021/day2"
 )
 
 func main() {
 	const (
 		dayOneInput string = "./day1/day1.txt"
+		dayTwoInput string = "./day2/day2.txt"
 	)
 
 	//count, err := day1.Day1P1(dayOneInput)
-	count, err := day1.Day1P22(dayOneInput)
-	if err != nil {
-		fmt.Printf("got err: %s\n", err)
-		return
-	}
+	//count, err := day1.Day1P22(dayOneInput)
+	count := day2.Day2P1(dayTwoInput)
 
 	fmt.Println("count was: ", count)
 }

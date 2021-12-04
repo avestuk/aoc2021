cc4bd3bdbfd0b74cfd4fdfc5c887c41c99ec6ae7
Alex Vest <alex.vest@nutmeg.com>
2021-12-04 09:39:56 +0000

    Start day4

---
+++ day4/day4.go
@@ -0,0 +1,9 @@
+package main
+
+func DrawNumbers() {
+}
+
+// Read input in sets of 5
+// Check for complete rows of 5
+// Continue
+// When a winner is found find all the "unmarked" numbers on the winning board(s)
